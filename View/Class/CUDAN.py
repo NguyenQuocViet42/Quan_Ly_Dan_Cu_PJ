3aa6b560e4abfbe48c8d8555cb805ebc5c2d91b7
NguyenQuocViet42 <112893487+NguyenQuocViet42@users.noreply.github.com>
2023-01-11 23:39:08 +0700

    Tạo, Xem kiến nghị, Sửa đăng nhập

--- View/Class/CUDAN.py
+++ View/Class/CUDAN.py
@@ -19,5 +19,21 @@ class CUDAN:
         self.NgayChuyenDi = NgayChuyenDi
         self.NoiChuyenDi = NoiChuyenDi
         self.GhiChu = GhiChu
-        
-    
+    def __init__(self, values):
+        self.ID = values[0]
+        self.CCCD = values[1]
+        self.Ten = values[2]
+        self.GioiTinh = values[3]
+        self.NgaySinh = values[4]
+        self.DanToc = values[5]
+        self.QuocTich = values[6]
+        self.NgheNghiep = values[7]
+        self.QueQuan = values[8]
+        self.BiDanh = values[9]
+        self.MaSo = values[10]
+        self.QuanHe = values[11]
+        self.NgayDangKyThuongtru = values[12]
+        self.DiaChiCu = values[13]
+        self.NgayChuyenDi = values[14]
+        self.NoiChuyenDi = values[15]
+        self.GhiChu = values[16]
